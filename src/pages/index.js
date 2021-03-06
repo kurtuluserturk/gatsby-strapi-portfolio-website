c8579f8b6f2a91ef6f74e43e4e26f75e4baba740
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-03-06 09:37:29 +0300

    blog components completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,24 +8,27 @@ import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
 export default ({ data }) => {  // data comes from graphql query
-  const { allStrapiProjects: { nodes: projects } } = data   // This destructing is the same pages/projects. We can compare them to udnerstand.
+
+  // This destructing is the same pages/projects. We can compare them to understand.
+  const {
+    allStrapiProjects: { nodes: projects },
+    allStrapiBlogs: { nodes: blogs }
+  } = data
 
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
-      <Projects
-        projects={projects}
-        title="featured projects"
-        showLink
-      />
+      <Projects projects={projects} title="featured projects" showLink />
+      <Blogs blogs={blogs} title="blog" showLink />
     </Layout>
   )
 }
 
 export const query = graphql`
-  {
+  {   
+
     allStrapiProjects(filter: {featured: {eq: true}}) {
       nodes {
         description
@@ -46,5 +49,25 @@ export const query = graphql`
         }
       }
     }
+
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+
   }
 `
